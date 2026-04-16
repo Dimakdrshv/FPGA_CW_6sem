cdab831c731ab956d6452e39403737a575f27a11
Dmitry Kudryashov <dimakudrashov13@gmail.com>
2026-04-16 15:50:01 +0300

    Created CW_REGFILE.v

---
+++ FPGA_CW_6sem.srcs/sources_1/new/CW_REGFILE.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 16.04.2026 15:45:30
+// Design Name: 
+// Module Name: CW_REGFILE
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module CW_REGFILE(
+
+    );
+endmodule
