08915f0eec5d211a4de7d7ac878b0e69c6ee7320
t4nyap4nina <t4nyap4nina@gmail.com>
2026-04-25 18:31:24 +0300

    created CW_CPU_INFS8B

---
+++ files/sources/CW_CPU_INFS8B.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 25.04.2026 18:29:46
+// Design Name: 
+// Module Name: CW_CPU_INFS8B
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module CW_CPU_INFS8B(
+
+    );
+endmodule
