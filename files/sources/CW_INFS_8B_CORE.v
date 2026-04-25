3dd4a408a68b95d9a6dcbbf586e50e5ead6a6f43
t4nyap4nina <t4nyap4nina@gmail.com>
2026-04-25 14:49:26 +0300

    create CW_INFS_8B_CORE

---
+++ files/sources/CW_INFS_8B_CORE.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 25.04.2026 14:39:27
+// Design Name: 
+// Module Name: CW_INFS_8B_CORE
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module CW_INFS_8B_CORE(
+
+    );
+endmodule
