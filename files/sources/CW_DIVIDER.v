ae31672e8191fe678514be7b27c5a6673cf84266
I-LOVE-GAMBLING-AND-BEER <sashsem11@gmail.com>
2026-04-27 00:11:08 +0300

    Update CW_DIVIDER.v
    Ну, может не работать. Все же я делал в точности соответствуя комментариям мистера К.

--- files/sources/CW_DIVIDER.v
+++ files/sources/CW_DIVIDER.v
@@ -1,29 +1,54 @@
 `timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: Semenikhin A.V.
+// 
+// Create Date: 26.04.2026 15:07:16
+// Design Name: 
+// Module Name: CW_DIVIDER
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: Frequency divider
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
 
 module CW_DIVIDER #(
-    parameter DIVIDER = 12)
+    parameter MOD = 12)
     (
     input      CLK,
     input      RST,
     output reg CE
     );
     
-    localparam CNT_WDT = $clog2(DIVIDER);
+    localparam CNT_WDT = $clog2(MOD);
     
     reg [CNT_WDT-1:0] CNT;
+
+	initial begin
+        CNT = {CNT_WDT{1'b0}};
+        CE  = 1'b0;
+    end
  
-    always @(posedge CLK)
-        if(RST) begin
+	always @(posedge CLK, posedge RST) begin
+        if (RST) begin
             CNT <= {CNT_WDT{1'b0}};
             CE <= 1'b0;
         end
         else begin
-            if(CNT == (DIVIDER - 1)) 
+            if (CNT == (MOD - 1)) 
                 CNT <= {CNT_WDT{1'b0}};
-            else 
+				CE = 1'b1
+			else (CE = 1'b0)
                 CNT <= CNT + 1;
-            CE <= (CNT == (DIVIDER - 1));
         end
+	end
         
 endmodule
 	
