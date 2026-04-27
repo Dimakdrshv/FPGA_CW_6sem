1707aaa61e8958ca0ce2a944aa61d3f06013fb7f
Banan_1984 <niketozzz@gmail.com>
2026-04-27 14:24:14 +0300

    add cw_ctrl_fsm

---
+++ files/sources/CW_CTRL_FSM.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/27/2026 11:20:44 AM
+// Design Name: 
+// Module Name: CW_CTRL_FSM
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module CW_CTRL_FSM(
+
+    );
+endmodule
