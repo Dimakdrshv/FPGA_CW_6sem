d99dfa02ef66fe5be378f5dd9b5467f950d8b9bd
Pomidor1638 <arzulum.ilyasov@gmail.com>
2026-05-01 00:37:46 +0300

    fix: fixed compiler.py and added .exe

--- software/compiler.py
+++ software/compiler.py
@@ -1,4 +1,5 @@
 import re
+import os
 import sys
 
 
@@ -9,7 +10,7 @@ pattern4  = r'^jmp \$[0-9a-f]{4}(\s*//.*)*$'
 pattern5  = r'^(jmps|jmpc) \$[0-9a-f]{4}, \$[01](\s*//.*)*$'
 pattern6  = r"^ldl r([0-9]|1[0-9]|2[0-9]|3[01]), \$[0-9a-f]{2}(\s*//.*)*$"
 pattern7  = r'^mov r([0-9]|1[0-9]|2[0-9]|3[01]), r([0-9]|1[0-9]|2[0-9]|3[01])(\s*//.*)*$'
-pattern8  = r"^ld r([0-9]|1[0-9]|2[0-9]|3[01]), (x|x\+|y|y\+)(\s*//.*)*$"
+pattern8  = r'^ld r([0-9]|1[0-9]|2[0-9]|3[01]), (x|x\+|y|y\+)(\s*//.*)*$'
 pattern9  = r'^stl (x|x\+|y|y\+), \$[0-9a-f]{2}(\s*//.*)*$'
 pattern10 = r'^st (x|x\+|y|y\+), r([0-9]|1[0-9]|2[0-9]|3[01])(\s*//.*)*$'
 pattern11 = r'^(reti|nop)(\s*//.*)*$'
@@ -19,42 +20,57 @@ def compile(cmd, n):
     cmd = cmd.lower().strip()
     result = 0
 
-    if cmd == "":
-        return None
+    if (
+        re.fullmatch(pattern1, cmd) is not None or
+        re.fullmatch(pattern2, cmd) is not None or
+        re.fullmatch(pattern3, cmd) is not None or
+        re.fullmatch(pattern4, cmd) is not None or
+        re.fullmatch(pattern5, cmd) is not None or
+        re.fullmatch(pattern6, cmd) is not None or
+        re.fullmatch(pattern7, cmd) is not None or
+        re.fullmatch(pattern8, cmd) is not None or
+        re.fullmatch(pattern9, cmd) is not None or
+        re.fullmatch(pattern10, cmd) is not None or
+        re.fullmatch(pattern11, cmd) is not None
+    ):
+        command = re.split(r',\s|\s|,|/', cmd)
 
-    if re.fullmatch(pattern1, cmd) != None or re.fullmatch(pattern2, cmd) != None or\
-       re.fullmatch(pattern3, cmd) != None or re.fullmatch(pattern4, cmd) != None or\
-       re.fullmatch(pattern5, cmd) != None or re.fullmatch(pattern6, cmd) != None or\
-       re.fullmatch(pattern7, cmd) != None or re.fullmatch(pattern8, cmd) != None or\
-       re.fullmatch(pattern9, cmd) != None or re.fullmatch(pattern10, cmd) != None or\
-       re.fullmatch(pattern11, cmd) != None:
-
-        command = re.split(r',\s|\s|//', cmd)
-
-        if command[0] == "add" or command[0] == "sub" or\
-                command[0] == "or" or command[0] == "and":
-            if(command[0] == "add"): result = 1 << 28
-            if(command[0] == "sub"): result = 2 << 28
-            if(command[0] == "or"):  result = 3 << 28
-            if(command[0] == "and"): result = 4 << 28
+        if (
+            command[0] == "add" or
+            command[0] == "sub" or
+            command[0] == "or" or
+            command[0] == "and"
+        ):
+            if command[0] == "add":
+                result = 1 << 28
+            if command[0] == "sub":
+                result = 2 << 28
+            if command[0] == "or":
+                result = 3 << 28
+            if command[0] == "and":
+                result = 4 << 28
 
             result |= int(command[1][1:]) << 19
 
-            if(command[2][0] == '$'):
+            if command[2][0] == '$':
                 result |= 1 << 27
                 result |= int(command[2][1:], 16)
             else:
                 result |= int(command[2][1:]) << 11
 
         elif command[0] == "inc" or command[0] == "dec":
-            if(command[0] == "inc"): result = 5 << 28
-            if(command[0] == "dec"): result = 6 << 28
+            if command[0] == "inc":
+                result = 5 << 28
+            if command[0] == "dec":
+                result = 6 << 28
 
             result |= int(command[1][1:]) << 19
 
         elif command[0] == "lsr" or command[0] == "rsr":
-            if(command[0] == "lsr"): result = 7 << 28
-            if(command[0] == "rsr"): result = 8 << 28
+            if command[0] == "lsr":
+                result = 7 << 28
+            if command[0] == "rsr":
+                result = 8 << 28
 
             result |= int(command[1][1:]) << 19
             result |= int(command[2][1:], 16) << 25
@@ -66,7 +82,7 @@ def compile(cmd, n):
         elif command[0] == "jmps" or command[0] == "jmpc":
             result = 10 << 28
 
-            if(command[0] == "jmpc"):
+            if command[0] == "jmpc":
                 result |= 1 << 24
 
             result |= int(command[1][1:], 16)
@@ -87,28 +103,40 @@ def compile(cmd, n):
             result = 12 << 28
             result |= int(command[1][1:]) << 19
 
-            if(command[2] == "x"): result |= 0 << 26
-            if(command[2] == "x+"): result |= 1 << 26
-            if(command[2] == "y"): result |= 2 << 26
-            if(command[2] == "y+"): result |= 3 << 26
+            if command[2] == "x":
+                result |= 0 << 26
+            if command[2] == "x+":
+                result |= 1 << 26
+            if command[2] == "y":
+                result |= 2 << 26
+            if command[2] == "y+":
+                result |= 3 << 26
 
         elif command[0] == "stl":
             result = 13 << 28
 
-            if(command[1] == "x"): result |= 0 << 26
-            if(command[1] == "x+"): result |= 1 << 26
-            if(command[1] == "y"): result |= 2 << 26
-            if(command[1] == "y+"): result |= 3 << 26
+            if command[1] == "x":
+                result |= 0 << 26
+            if command[1] == "x+":
+                result |= 1 << 26
+            if command[1] == "y":
+                result |= 2 << 26
+            if command[1] == "y+":
+                result |= 3 << 26
 
             result |= int(command[2][1:], 16)
 
         elif command[0] == "st":
             result = 14 << 28
 
-            if(command[1] == "x"): result |= 0 << 26
-            if(command[1] == "x+"): result |= 1 << 26
-            if(command[1] == "y"): result |= 2 << 26
-            if(command[1] == "y+"): result |= 3 << 26
+            if command[1] == "x":
+                result |= 0 << 26
+            if command[1] == "x+":
+                result |= 1 << 26
+            if command[1] == "y":
+                result |= 2 << 26
+            if command[1] == "y+":
+                result |= 3 << 26
 
             result |= int(command[2][1:]) << 11
 
@@ -119,54 +147,48 @@ def compile(cmd, n):
             result = 0
 
     else:
-        raise Exception("Некорректная команда {0}:{1}".format(n, cmd))
+        raise Exception("Command violation {0}:{1}".format(n, cmd))
 
     return result
 
-
-def save_coe(in_path, out_path, size_mem=1024):
+def save_mem(in_path, out_path, name, size_mem):
     n = 1
     prgmem = []
 
     with open(in_path, 'r') as f:
         for line in f:
-            code = compile(line, n)
-
-            if code != None:
-                prgmem.append(format(code, '08X'))
-
+            prgmem.append(format(compile(line, n), '08X') + "\n")
             n += 1
 
-    if len(prgmem) > size_mem:
-        raise Exception("Размер программы больше размера памяти: {0}>{1}".format(len(prgmem), size_mem))
+    for i in range(size_mem - len(prgmem)):
+        if len(prgmem) != size_mem - 1:
+            prgmem.append("00000000\n")
+        else:
+            prgmem.append("00000000")
 
-    while len(prgmem) < size_mem:
-        prgmem.append('00000000')
+    with open(os.path.join(out_path, name + ".mem"), "w+") as f:
+        f.writelines(prgmem)
 
-    with open(out_path, "w+") as f:
-        f.write("memory_initialization_radix=16;\n")
-        f.write("memory_initialization_vector=\n")
-
-        for i in range(len(prgmem)):
-            if i == len(prgmem) - 1:
-                f.write(prgmem[i] + ";\n")
-            else:
-                f.write(prgmem[i] + ",\n")
+def run_compiler(in_path, out_path, name, size_mem):
+    save_mem(in_path, out_path, name, int(size_mem))
 
 
-if __name__ == "__main__":
-    if len(sys.argv) < 3:
-        print("Использование:")
-        print("python compiler.py input.asm output.coe")
-        print("python compiler.py input.asm output.coe 1024")
-        sys.exit(1)
+def main():
+    if len(sys.argv) != 5:
+        print("Usage:")
+        print("  assembler.exe <input_file> <output_path> <name> <size_mem>")
+        return
 
     in_path = sys.argv[1]
     out_path = sys.argv[2]
+    name = sys.argv[3]
+    size_mem = int(sys.argv[4])
 
-    if len(sys.argv) >= 4:
-        size_mem = int(sys.argv[3])
-    else:
-        size_mem = 1024
+    run_compiler(in_path, out_path, name, size_mem)
 
-    save_coe(in_path, out_path, size_mem)
+    print("File assembled successfully:")
+    print(out_path + "/" + name + ".mem")
+
+
+if __name__ == "__main__":
+    main()
