00ef52573c60d4c5a79fb500b248c53eb3f8cd8a
Dmitry Kudryashov <dimakudrashov13@gmail.com>
2026-05-07 17:38:30 +0300

    fix  logic

--- files/sources/CW_DIVIDER_CASCADE.v
+++ files/sources/CW_DIVIDER_CASCADE.v
@@ -30,22 +30,23 @@ module CW_DIVIDER_CASCADE
     input  wire CE,
     output reg  CEO
 );
-
-    reg [$clog2(MOD) - 1 : 0] CNT;
+    
+    localparam CNT_WDT = $clog2(MOD);
+    reg [CNT_WDT - 1 : 0] CNT;
     
     initial begin
-        CNT = {($clog2(MOD)){1'b0}};
+        CNT = {(CNT_WDT){1'b0}};
         CEO = 1'b0;
     end
     
     always @(posedge CLK, posedge RST) begin
         if (RST) begin
-            CNT <= {($clog2(MOD)){1'b0}};
+            CNT <= {(CNT_WDT){1'b0}};
             CEO <= 1'b0;
         end else begin
             if (CE) begin
                 if (CNT == (MOD - 1)) begin
-                    CNT <= {($clog2(MOD)){1'b0}};
+                    CNT <= {(CNT_WDT){1'b0}};
                 end else begin
                     CNT <= CNT + 1'b1;
                 end
